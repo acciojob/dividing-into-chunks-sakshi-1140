aade016f7fb284c6b1f87ddf1e64f4d941fb5941
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-05-23 15:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,33 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+//all subarray
+//sum for all
+//if sum<n then push
+let result=[]
+let maxSum=n
+//console.log(arr);
+for(let i=0;i<arr.length;i++)
+{
+  for(let j=i+1;j<arr.length+1;j++)
+  { 
+     let sum=0
+     let subArr=arr.slice(i,j)
+     sum=Sum(subArr);
+  if(sum<=maxSum) result.push(subArr)
+  // console.log(subArr)
+  }
+}
+console.log(result)
+//console.log(arr);
+
+function Sum(arr)
+{
+  let sum=0
+  for(let t of arr) sum+=t
+  return sum;
+}	
+	
 };
 
 const n = prompt("Enter n: ");
